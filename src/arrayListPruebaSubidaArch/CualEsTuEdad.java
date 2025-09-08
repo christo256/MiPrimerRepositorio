127ba3843c0ef240c5bd4843ebe2f667389c7435
christo256 <ckingmaster2@gmail.com>
2025-09-08 13:08:08 +0200

    Update CualEsTuEdad.java

--- src/arrayListPruebaSubidaArch/CualEsTuEdad.java
+++ src/arrayListPruebaSubidaArch/CualEsTuEdad.java
@@ -7,30 +7,7 @@ public class CualEsTuEdad {
         // TODO Auto-generated method stub
 
 		/*
-		 *  Ejercicio 3: ¿Cuál es tu edad?
-�
-�	Objetivo:	Pedir	al	usuario	que	ingrese	edades	y	clasificar	si	es	menor	o	mayor	de	edad
-con	if.
-
-�
-�	Explicación:
-Pediremos	al	usuario	que	ingrese	varias	edades	y	las	guardaremos	en	un
-ArrayList<Integer>.	Luego,	recorreremos	la	lista	con	un	for	y	usaremos	un	if	para	verificar
-si	la	edad	es	menor	de	18	o	mayor.
-
-�
-�	Ejemplo	de	entrada:
-[12,	20,	8,	25]
-
-�
-�	Salida	esperada:
-Edad:	12	→	Eres	menor	de	edad	🚸
-Edad:	20	→	Eres	mayor	de	edad	🎉
-Edad:	8	→	Eres	menor	de	edad	🚸
-Edad:	25	→	Eres	mayor	de	edad	🎉
-�
-�	Extra:	Usa	un	contador	para	saber	cuántas	personas	son	menores	y	cuántas	mayores	de
-edad.
+		 *  MODIFICACION PARA PROBAR SI PUEDO SUBIR EL ARCHIVO DE LA NUVE AL PC LOCAL
 		 */
 
 
